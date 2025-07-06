9f2585d028d742a7e0709918e2778d7aeaa7b25e
tsiro66 <116112100+tsiro66@users.noreply.github.com>
2025-07-06 13:58:34 +0300

    Added fb pixel

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,42 @@ useEffect(() => {
 },[]);
 
 
+// Add this to your main App component or a custom hook
 useEffect(() => {
-    if (window.fbq) {
-      window.fbq('track', 'PageView');
-    }
-  }, [location]);
+  // Load Facebook Pixel
+  const script = document.createElement('script');
+  script.innerHTML = `
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1908509646610367');
+    fbq('track', 'PageView');
+  `;
+  document.head.appendChild(script);
+
+  // Add noscript fallback
+  const noscript = document.createElement('noscript');
+  noscript.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1908509646610367&ev=PageView&noscript=1" />';
+  document.body.appendChild(noscript);
+
+  return () => {
+    // Cleanup if needed
+    document.head.removeChild(script);
+    document.body.removeChild(noscript);
+  };
+}, []);
+
+// Then track route changes
+useEffect(() => {
+  if (window.fbq) {
+    window.fbq('track', 'PageView');
+  }
+}, [location]);
 
   const handleSubmit = async (e) => {
   e.preventDefault();
